38b9bc87b5c79bc7109c51019deb9782b16c13d4
Daniel Pawlak <daniel@Daniels-iMac.local>
2021-02-19 15:54:55 -0800

    Fixed GitHub security vulnerability message.

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBLDvFf-wBrOS-gwcElkPeujQ0KJVR3GzY",
+    authDomain: "whatsapp-mern-19eaa.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-19eaa.firebaseio.com",
+    projectId: "whatsapp-mern-19eaa",
+    storageBucket: "whatsapp-mern-19eaa.appspot.com",
+    messagingSenderId: "815496995320",
+    appId: "1:815496995320:web:fef61a4acced9ab375360d"
+  };
